a3959bfd95cadfba4e144b5f689a066d5dd78bec
hessium <hessium56@gmail.com>
2025-04-26 19:41:13 +0300

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,11 @@ import { WeatherCity } from './components/weather-city/weather-city.tsx';
 import { PopularCities } from './components/popular-cities/popular-cities.tsx';
 import { MainLayout } from './containers/main-layout/main-layout.tsx';
 import { MainSection } from './containers/main-section/main-section.tsx';
-import { Spinner } from './shared/UI/spinner/spinner.tsx';
+
 function App() {
   return (
     <MainLayout>
       <AppHeader />
-      <Spinner />
       <MainSection>
         <WeatherCity />
       </MainSection>
